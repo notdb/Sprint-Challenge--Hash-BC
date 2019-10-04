1ddff0bdf7619a887445a15d8f24f47bdb1d6fbc
Doug <me@douglasbowen.com>
2019-10-04 13:36:04 -0400

    add ex1.py with passing tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -10,17 +10,21 @@ def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
     """
-    YOUR CODE HERE
+    # take the limit, subtract it by everything in the table once
+    # then take the reset, query the hash table to see if it's there
+    # for example, 21 - 4 = 17, so we see if 17 is in the hash table, if it is, we know 4 and 17 are our answers
     """
+    if len(weights) == 1:
+        return None
     for weight in weights:
         hash_table_insert(ht, weight, weights.index(weight))
-        
-    y = 0
-    while y != 4:
-        for x in range(len(weights)-y):
-            print(x)
-        y += 1
-    return hash_table_retrieve(ht, 16)
+        if hash_table_retrieve(ht, limit - weight) is not None:
+            if weights.index(weight) == hash_table_retrieve(ht, limit - weight):
+                newList =[weights.index(weight)+1, hash_table_retrieve(ht, limit - weight)]
+            else:
+                newList = [weights.index(weight), hash_table_retrieve(ht, limit - weight)]
+            
+    return newList
 
 def print_answer(answer):
     if answer is not None:
@@ -28,18 +32,6 @@ def print_answer(answer):
     else:
         print("None")
 
-
-get_indices_of_item_weights([ 4, 6, 10, 15, 16 ], 5, 21)
-
-46
-410
-415
-416
-610
-615
-616
-1015
-1016
-1516
-
+#get_indices_of_item_weights([ 4, 6, 10, 15, 16 ], 5, 21)
 # a for loop that starts at the length of the array minus 1, starts basically you need to take the first one, add the rest, second, add the rest, third, add the rest until you find a sum that equals the limit and stop, could be done recursively
+
