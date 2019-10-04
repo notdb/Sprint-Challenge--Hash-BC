a319122ef332dd8834d366053fa1a98c06da9735
Doug <me@douglasbowen.com>
2019-10-04 13:02:53 -0400

    add mock solution for hashtable exercise 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,12 +12,34 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-
-    return None
-
+    for weight in weights:
+        hash_table_insert(ht, weight, weights.index(weight))
+        
+    y = 0
+    while y != 4:
+        for x in range(len(weights)-y):
+            print(x)
+        y += 1
+    return hash_table_retrieve(ht, 16)
 
 def print_answer(answer):
     if answer is not None:
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
+
+get_indices_of_item_weights([ 4, 6, 10, 15, 16 ], 5, 21)
+
+46
+410
+415
+416
+610
+615
+616
+1015
+1016
+1516
+
+# a for loop that starts at the length of the array minus 1, starts basically you need to take the first one, add the rest, second, add the rest, third, add the rest until you find a sum that equals the limit and stop, could be done recursively
